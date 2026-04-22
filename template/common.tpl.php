77f2300f3a0b4bbfd95e909fc58864b84b5a2dae
luislima9970 <pt.luis.lima2006@gmail.com>
2026-04-22 11:49:41 +0100

    First testing with php

---
+++ template/common.tpl.php
@@ -0,0 +1,48 @@
+<?php 
+    function generateHead() {
+       return ' <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="html/style.css">
+    <title>Gym</title>
+</head>';
+    }
+    function generateHeader() { 
+
+    return  '<header>
+        <label for="menu-check" id="menu-icon">
+        &#9776;
+        </label>
+        <a href="index.html"><img src="https://fastly.picsum.photos/id/180/50/50.jpg?hmac=XMW9Xfp3fH-OoI44sBBOzJib8r8soPYbeUHO0d-OT20" alt="logo" width="50" height="50"></a>
+         <input type="checkbox" id="menu-check">
+
+    
+        <nav>
+            <ul>
+                <li><a href="index.html">About us</a></li>
+                <li><a href="index.html">Classes</a></li>
+                <li><a href="index.html">Trainers</a></li>
+                <li><a href="index.html">Equipment</a></li>
+            </ul>
+        </nav>
+        <div id="signup">
+        <a href="register.html">Register</a>
+        <a href="login.html">Login</a>
+      </div>
+    </header>';
+
+
+    }
+
+    function generateFooter() { 
+
+    return '<footer class="main-footer">
+        <p>Copyright &copy; All right reserved</p>
+        <p>Rua njdfbajlfhak 00, 4000-000 Cidade | email@powerpit.com | +555 900 000 000</p>
+        <p>Our transactions are made in euros (&euro;).</p>
+    </footer>';
+
+    }
+
---
+++ template/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/common.tpl.php';
+?>
+
+<?= generateHead() ?>
+<body>
+    <?= generateHeader() ?>
+
+    <?= generateFooter() ?>
+</body>
+</html>
