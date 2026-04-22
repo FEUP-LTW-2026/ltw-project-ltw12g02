81530b15b4c930e15252aaef1aa179235a507e5d
gabriielaaf <gabriela.abreu.felgueiras@gmail.com>
2026-04-22 23:04:21 +0100

    table trainers implemented

--- database/database.sql
+++ database/database.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Trainers;
 
 /*******************************************************************************
    Create Tables
@@ -12,5 +13,26 @@ CREATE TABLE Users{
    PasswordHash NVARCHAR(255) NOT NULL,
    ProfilePhoto NVARCHAR(255),
    Role NVARCHAR(20) NOT NULL,
+
+   PRIMARY KEY (UserId),
+   UNIQUE (Username),
+   UNIQUE (Email)
 };
 
+CREATE TABLE Trainers{
+   TrainerId INTEGER NOT NULL,
+   UserId INTEGER NOT NULL,
+   Bio NVARCHAR(500),
+   Specializations NVARCHAR(255),
+   Certifications NVARCHAR(255),
+
+   PRIMARY KEY (TrainerId),
+   UNIQUE (UserId),
+   FOREIGN KEY (UserId) REFERENCES Users (UserId) ON DELETE NO ACTION ON UPDATE NO ACTION
+};
+
+/*******************************************************************************
+   Create Foreign Keys
+********************************************************************************/
+
+CREATE INDEX IF NOT EXISTS IFK_TrainersUserId ON Trainers (UserId);
